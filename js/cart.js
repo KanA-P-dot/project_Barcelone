4930e462a214bd79352e7f9c4daff80ae9fa6bfa
Kan-à-Pé <62486446+KanA-P-dot@users.noreply.github.com>
2023-10-17 10:57:42 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,38 @@
-test
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const addToCartButtons = document.querySelectorAll(".add-to-cart");
+    const cartItemsList = document.getElementById("cart-items");
+    const cartTotal = document.getElementById("cart-total");
+    const checkoutButton = document.getElementById("checkout");
+    const cart = [];
+
+    addToCartButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const product = button.getAttribute("data-product");
+            const price = parseFloat(button.getAttribute("data-price"));
+
+            cart.push({ product, price });
+            updateCart();
+        });
+    });
+
+    checkoutButton.addEventListener("click", () => {
+        alert("Thank you for your purchase!");
+        cart.length = 0; // Clear the cart
+        updateCart();
+    });
+
+    function updateCart() {
+        cartItemsList.innerHTML = "";
+        let total = 0;
+
+        cart.forEach((item) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${item.product} - €${item.price}`;
+            cartItemsList.appendChild(listItem);
+            total += item.price;
+        });
+
+        cartTotal.textContent = total.toFixed(2);
+    }
+});
